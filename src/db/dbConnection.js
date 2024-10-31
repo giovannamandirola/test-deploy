f2dca7c17de25eb8f7a0dc5c4b6f4494c7dc086e
Giovanna Mandirola <GiovannaMandirola@IBM-PF3HS9DC>
2024-10-31 19:49:01 -0300

    .

--- src/db/dbConnection.js
+++ src/db/dbConnection.js
@@ -5,7 +5,7 @@ const sequelize = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASS,
   {
-    host: process.env.DB_HOST,
+    host: 'mysql',
     port: process.env.DB_LOCAL_PORT,
     dialect: 'mysql',
     logging: false
