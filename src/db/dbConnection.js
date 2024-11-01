07302577a653a38139a6d59887a06fc73611f569
Giovanna Mandirola <GiovannaMandirola@IBM-PF3HS9DC>
2024-11-01 12:18:10 -0300

    Change on db, delete dev action

--- src/db/dbConnection.js
+++ src/db/dbConnection.js
@@ -7,12 +7,12 @@ const sequelize = new Sequelize(
   {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    dialect: 'mysql',  
+    dialect: 'mysql',
     dialectOptions: {
       ssl: {
         require: true,
-        rejectUnauthorized: false,
-      },
+        rejectUnauthorized: false
+      }
     },
     logging: false
   }
